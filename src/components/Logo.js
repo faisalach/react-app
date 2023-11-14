fdc6f420ecb5134f70d5be7118d570b459857f02
faisalach <faisalachramsyah@gmail.com>
2023-11-14 08:35:36 +0700

    change color logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = ({color}) => {
     return (
         <>
             <div className="flex items-center">
-                <FontAwesomeIcon icon={faGears} className="h-8 mr-3 text-gray-700" />
+                <FontAwesomeIcon icon={faGears} className="h-8 mr-3 text-sky-300" />
                 <div className="h-8">
                     <p style={{lineHeight:'20px'}} className={`p-0 m-0 self-center text-md font-semibold whitespace-nowrap ${color}`}>Sistem Manajemen</p>
                     <p style={{lineHeight:'12px'}} className={`self-center text-sm whitespace-nowrap ${color}`}>Kendaraan</p>
